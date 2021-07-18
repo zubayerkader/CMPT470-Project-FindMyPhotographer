de48e868d16aff135d71c248d0318aeae03b67c8
Arian Ansari <esfahanarian@gmail.com>
2021-07-18 01:38:46 -0700

    navbar

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,17 @@
+export const MenuItems = [
+{
+    title: 'Home',
+    url: '#',
+    cName: 'nav-links'
+},
+{
+    title: 'Services',
+    url: '#',
+    cName: 'nav-links'
+},
+{
+    title: 'Contact',
+    url: '#',
+    cName: 'nav-links'
+},
+]
