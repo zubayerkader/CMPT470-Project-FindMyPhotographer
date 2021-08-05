3b0bbc2337a05b855be9faad6c3c70d8612ade5e
Kazi Zubayer Quader Dhrubo <zubayerkader@gmail.com>
2021-08-05 16:39:15 -0700

    fixed profile apis

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -4,96 +4,32 @@ const Photographer = require("../models/photographerModel");
 const Review = require("../models/reviewModel");
 
 
-// localhost:4000/profiles/photographer/60f5b295c9554f57cd249d8b
-// exports.getProfile = (req, res, next)=>{
-//     let id = req.params.id;
-//     let type = req.params.userType;
-//     console.log(type)
-//     console.log(id)
-
-//     let querySchema;
-//     let returnData = {
-//         // personalInfo: {},
-//         // bookingInfo: {},
-//     }
-
-//     if (type === "photographer"){
-//         querySchema = Photographer;
-//     }
-//     else if (type === "client") {
-//         querySchema = Client;
-//     }
-//     else{
-//         res.status(500).json(`Not a valid user type: ${type}`);
-//         return
-//     }
-//     querySchema.findById(id, function(err, result){
-//         if (err){
-//             res.status(500).json(err);
-//             return;
-//         }
-//         else{
-//             returnData.personalInfo = result;
-//             // res.json(returnData);
-//         }
-//     }).then( ()=>{
-//         console.log(type)
-//         console.log(id)
-//         Booking.find({[type]: id}, function(err, result){
-//             if (err){
-//                 res.status(500).json(err);
-//                 return;
-//             }
-//             else{
-//                 returnData.bookingInfo = result;
-//                 // res.json(returnData);
-//             }
-//         });   
-//     }).then( ()=>{
-//         Review.find({photographer: id}, function(err, result){
-//             if (err){
-//                 res.status(500).json(err);
-//                 return;
-//             }
-//             else{
-//                 returnData.reviewInfo = result;
-//                 res.json(returnData);
-//             }
-//         })
-//     }
-//     )
-// };
+// http://localhost:4000/profiles/photographer/60f5b295c9554f57cd249d8b
+// http://localhost:5000/profiles/client/60f5b60e677fb859444c4eae
 
 exports.getClientProfile = (req, res, next)=>{
     let id = req.params.id;
     console.log(id)
 
     let returnData = {
-        // personalInfo: {},
-        // bookingInfo: {},
+        personalInfo: {},
+        bookingInfo: {},
     }
-    Client.findById(id, function(err, result){
-        if (err){
-            res.status(500).json(err);
-            return;
-        }
-        else{
-            returnData.personalInfo = result;
-            // res.json(returnData);
-        }
-    }).then( ()=>{
-        console.log(id)
-        Booking.find({client: id}, function(err, result){
-            if (err){
-                res.status(500).json(err);
-                return;
-            }
-            else{
-                returnData.bookingInfo = result;
-                res.json(returnData);
-            }
-        });   
-    })};
+    
+    Client.findById(id)
+    .then((personalInfo)=>{
+        returnData.personalInfo = personalInfo;
+        return Booking.find({client: id});
+    })
+    .then((bookings)=>{
+        returnData.bookingInfo = bookings;
+        return res.json(returnData);
+    })
+    .catch((err)=>{
+        if (!err.statusCode) err.statusCode = 500;
+          next(err);
+    });
+};
 
 
 exports.getPhotographerProfile = (req, res, next)=>{
@@ -101,43 +37,28 @@ exports.getPhotographerProfile = (req, res, next)=>{
     console.log(id)
 
     let returnData = {
-        // personalInfo: {},
-        // bookingInfo: {},
+        personalInfo: {},
+        bookingInfo: {},
+        reviewInfo: {},
     }
-    Photographer.findById(id, function(err, result){
-        if (err){
-            res.status(500).json(err);
-            return;
-        }
-        else{
-            returnData.personalInfo = result;
-            // res.json(returnData);
-        }
-    }).then( ()=>{
-        console.log(id)
-        Booking.find({photographer: id}, function(err, result){
-            if (err){
-                res.status(500).json(err);
-                return;
-            }
-            else{
-                returnData.bookingInfo = result;
-                // res.json(returnData);
-            }
-        });   
+    
+    Photographer.findById(id)
+    .then((personalInfo)=>{
+        returnData.personalInfo = personalInfo;
+        return Booking.find({photographer: id});
     })
-    .then( ()=>{
-        Review.find({photographer: id}, function(err, result){
-            if (err){
-                res.status(500).json(err);
-                return;
-            }
-            else{
-                // console.log(result)
-                returnData.reviewInfo = result;
-                res.json(returnData);
-            }
-        })
-    }
-    )
+    .then((bookings)=>{
+        returnData.bookingInfo = bookings;
+        return Review.find({photographer: id});
+    })
+    .then((reviews)=>{
+        returnData.reviewInfo = reviews;
+        return res.json(returnData);
+    })
+    .catch((err)=>{
+        if (!err.statusCode) err.statusCode = 500;
+          next(err);
+    });
+
+
 };
